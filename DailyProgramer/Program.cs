833e9309cedaca6a15e6280d713a9c10d9958668
Brendan Caffrey <brendan@devspade.com>
2016-05-18 21:36:33 -0400

    completed dog contest

--- DailyProgramer/Program.cs
+++ DailyProgramer/Program.cs
@@ -1,4 +1,5 @@
-﻿using DailyProgramer.Permutations;
+﻿using DailyProgramer.DogContest;
+using DailyProgramer.Permutations;
 using System;
 using System.Linq;
 
@@ -10,12 +11,19 @@ namespace DailyProgramer
         {
 
             var command = GetInput("Enter a command:");
-            var validCommands = new[] {"permuation","combination" };
+            var validCommands = new[] {"permuation","combination","dog" };
 
             if (!validCommands.Contains(command)) throw new ArgumentException("Not a valid command.");
 
             try {
-
+                if(command == "permuation" || command == "combination")
+                {
+                    PermutationsAndCombinations(command);
+                }
+                if(command == "dog")
+                {
+                    RunDogContest();
+                }
             }
             catch(Exception exception)
             {
@@ -41,7 +49,7 @@ namespace DailyProgramer
         /// https://www.reddit.com/r/dailyprogrammer/comments/4hhiu8/20160502_challenge_265_easy_permutations_and/
         /// </summary>
         /// <param name="command"></param>
-        private static void PermuationsAndCombinations(string command)
+        private static void PermutationsAndCombinations(string command)
         {
             var factor = GetInputAsInt("How big a tree?");
             if (command == "permutation")
@@ -69,6 +77,16 @@ namespace DailyProgramer
                 var answer = tree.ElementAt(index - 1);
                 Console.WriteLine("The {0} combination number of {1} out of {2} us {3}", index, size, factor, answer);
             }
+        }
+
+        /// <summary>
+        /// https://www.reddit.com/r/dailyprogrammer/comments/4jom3a/20160516_challenge_267_easy_all_the_places_your/
+        /// </summary>
+        private static void RunDogContest()
+        {
+            var totalDogs = GetInputAsInt("How many dogs are in the contest?");
+            var myDog = GetInputAsInt("What place was your dog?");
+            Contest.Run(totalDogs, myDog);
         }
     }
 }
